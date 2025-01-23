bb4be4832c30d6f2ea2dc307fd86905608ff1d60
garvbahal <garvbahal75@gmail.com>
2025-01-23 12:59:16 +0530

    third-commit

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,10 +1,58 @@
 const Posts = require("../models/Posts");
 
-exports.createComment = async(req,res)=>{
-  try{
-    
-  }
-  catch(err){
+exports.createComment = async (req, res) => {
+  try {
+    const { comment } = req.body;
+    const id = req.params.id;
+    const response = await Posts.findByIdAndUpdate(
+      { _id: id },
+      { comment: comment },
+      { new: true }
+    );
 
+    if (!response) {
+      return res.status(404).json({
+        success: false,
+        message: "Id not Found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: response,
+      message: "Comment Created Successfully",
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      error: err.message,
+      message: "Internal Server Error",
+    });
   }
-}
+};
+
+exports.retrieveComment = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const response = await Posts.findById(id);
+    
+    if (!response) {
+      return res.status(404).json({
+        success: false,
+        message: "Id not Found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: response.comment,
+      message: "Comment Retrieved Successfully",
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      error: err.message,
+      message: "Internal Server Error",
+    });
+  }
+};
--- routes/posts.js
+++ routes/posts.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const { createPost, retrievePosts } = require("../controllers/posts");
 const { like, unlike } = require("../controllers/likes");
+const { createComment, retrieveComment } = require("../controllers/comments");
+
 router.post("/posts/create", createPost);
 
 router.get("/posts", retrievePosts);
@@ -10,5 +12,9 @@ router.get("/posts", retrievePosts);
 router.put("/likes/like/:id", like);
 
 router.put("/likes/unlike/:id", unlike);
+
+router.post("/comments/create/:id", createComment);
+
+router.get("/comments/:id", retrieveComment);
 
 module.exports = router;
